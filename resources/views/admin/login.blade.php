2fc12da72e1704535a05cf18ec82b6b2b3f70e08
= <=>
2021-05-28 22:10:41 +0700

    tampilan login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,12 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.7 -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/dashboard/bower_components/bootstrap/dist/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/dashboard/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('assets/dashboard/bower_components/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('assets/dashboard/bower_components/font-awesome/css/font-awesome.min.css') }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="{{ asset('assets/dashboard/bower_components/Ionicons/css/ionicons.min.css') }}">
     <!-- Theme style -->
@@ -23,35 +27,57 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
 </head>
-<body class="hold-transition login-page">
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark ">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Kedai Assalamualaikum
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item control-label">
+                            <a class="nav-link " href="/login">Login User</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
     <div class="login-box">
         <div class="login-logo">
             <a href=""><b>Admin</b>ISTRATOR</a>
         </div>
-        <div class="login-box-body">
+        <div>
             <p class="login-box-msg">Sign in to start your session</p>
             <form action="{{ route('postLogin') }}" method="post">
                 {{ csrf_field() }}
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} has-feedback">
-                    <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required autofocus>
+                    <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}"
+                        required autofocus>
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
 
                     @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
                     @endif
                 </div>
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} has-feedback">
-                    <input type="password" name="password" class="form-control" placeholder="Password" required="required">
+                    <input type="password" name="password" class="form-control" placeholder="Password"
+                        required="required">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
 
                     @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
                     @endif
                 </div>
                 <div class="row">
@@ -77,13 +103,14 @@
     <!-- iCheck -->
     <script src="{{ asset('assets/dashboard/plugins/iCheck/icheck.min.js') }}"></script>
     <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%'
-            });
+    $(function() {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%'
         });
+    });
     </script>
 </body>
+
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,78 +1,97 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- <div class="text-center"> -->
 <div class="container">
-    <div class="row">
+
+    <div class="row justify-content-center">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+                <center>
+                    <!-- <div class="panel-heading">Register</div> -->
+                    <div class="panel-body">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                            {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" placeholder="name" class="form-control" name="name"
+                                        value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                    @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <!-- <label for="email" class="col-md-4 control-label">E-Mail Address</label> -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" placeholder="Email" class="form-control" name="email"
+                                        value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
+                                    @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <!-- <label for="password" class="col-md-4 control-label">Password</label> -->
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" placeholder="Password" class="form-control"
+                                        name="password" required>
 
-                                @if ($errors->has('password'))
+                                    @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <div class="form-group">
+                                <!-- <label for="password-confirm" class="col-md-4 control-label">Confirm
+                                    Password</label> -->
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" placeholder="Confirm password"
+                                        class="form-control" name="password_confirmation" required>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
+                            <div class="row">
+                                <div class="col-5 offset-2">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember"
+                                                {{ old('remember') ? 'checked' : ''}}>
+                                            Remember Me
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-2 ">
+                                    <button type="submit" class="btn btn-primary">
+                                        Register
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                    <br><center><p>Kedai Assalamualaikum 2021</p></center>
-                    
-                </div>
+                        </form>
+                        <br>
+                        <center>
+                            <p>Kedai Assalamualaikum 2021</p>
+                        </center>
+                </center>
             </div>
         </div>
     </div>
+</div>
+<!-- </div> -->
 </div>
 @endsection
